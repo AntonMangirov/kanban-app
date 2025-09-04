dd08b50a689a703ea01506ddbf36e6dbf00ce88f
rignam <antonmangirov@gmail.com>
2025-09-04 16:39:41 +0300

    rendering the dnd container

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,15 @@
+'use client';
+
+import { DndContext } from '@dnd-kit/core';
+
+export default function DashboardPage() {
+  return (
+    <DndContext>
+      <div className="flex flex-row gap-4 overflow-x-auto">
+        {}
+      </div>
+    </DndContext>
+  );
+}
+
+
